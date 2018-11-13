c1b2a25f343f106257f4eba2d3bae4b66e9e6bf2
Daniel Spang <daniel.spang@distilnetworks.com>
2018-11-13 11:50:40 +0100

    b

--- currentscript.js
+++ currentscript.js
@@ -1,1 +1,7 @@
-cs()
+(function() {
+    var scripts = document.getElementsByTagName('script');
+    var script = scripts[scripts.length - 1];
+    console.log(document.currrentScript);
+    console.log("currentScript: " + document.currrentScript.src + " last scripttag: " + script.src)
+})()
+
