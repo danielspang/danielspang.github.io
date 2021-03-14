9eb4bdcc59c540db46dc3e59fbe664e55b79829d
Daniel Spang <spangds@amazon.com>
2021-03-14 16:44:18 +0100

    Update

--- mult/mult.js
+++ mult/mult.js
@@ -7,7 +7,23 @@ function update(res) {
         var next = res + c;
     }
     document.getElementsByTagName("div")[0].textContent = res;
-    window.setTimeout(update, 3000, next)
+    window.setTimeout(update, 1000 * time(a,b), next)
+}
+
+function time(a, b) {
+    if (!a) {
+        return 4;
+    }
+    var easy = [1,2,10];
+    var medium = [3,4,5];
+    var hard = [6,7,8,9];
+    if (easy.includes(a) || easy.includes(b)) {
+        return 3;
+    } else if (medium.includes(a) && medium.includes(b)) {
+        return 4;
+    } else {
+        return 6;
+    }
 }
 
 update();
