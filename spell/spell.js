c4d79942cdf8791f5f7e331a302299d4784e264b
Daniel Spang <spangds@amazon.com>
2021-09-25 14:52:51 +0200

    Add spell

---
+++ spell/spell.js
@@ -0,0 +1,62 @@
+var synth = window.speechSynthesis;
+var voices = synth.getVoices();
+
+for (var i = 0; i < voices.length; i++) {
+    if (voices[i].lang == 'sv-SE') {
+        var voice = voices[i];
+    }
+}
+
+async function say(what) {
+    var synth = window.speechSynthesis;
+    var utterThis = new SpeechSynthesisUtterance(what);
+    utterThis.voice = voice;
+    synth.speak(utterThis);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function start() {
+    word = WORDS[Math.floor((Math.random() * 1000))];
+    say(word);
+}
+
+async function fel(str) {
+    let p = document.querySelector('p');
+    say("Fel. Försök igen.");
+    p.innerText = "\u00a0";
+    for (c of word) {
+        await sleep(500);
+        p.innerText += c;
+    }
+    await sleep(2000);
+    let input = document.querySelector('input');
+    input.value = '';
+    p.innerText = "\u00a0"
+    input.focus();
+}
+
+var inputForm = document.querySelector('form');
+inputForm.onsubmit = function (event) {
+    var inputTxt = document.querySelector('input');
+    if (inputTxt.value == word) {
+        say("Rätt");
+        inputTxt.value = "\u00a0";
+        //inputTxt.blur();
+        start();
+    } else {
+        fel(word);
+
+    }
+    event.preventDefault();
+}
+
+document.querySelector('#skip_word').onclick = function (event) {
+    document.querySelector('p').innerText = "\u00a0"
+    start();
+    event.preventDefault();
+}
+
+start()
